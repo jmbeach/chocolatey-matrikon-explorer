3536e834116c17c6659696ee38804fe450a20c38
Jared Beach <j.beach@hylasoftusa.com>
2017-03-09 15:28:06 -0600

    init

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'matrikon-explorer'
+$fileName = 'MobileOPCExplorer.zip'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$zipPath = "$toolsPath\$fileName"
+
+Get-ChocolateyUnzip -PackageName $packageName -FileFullPath $zipPath -Destination $toolsPath
+rm $zipPath -ea 0
+
+# create alias
+
+$N = "`r`n"
+
+$batch = "@echo off"+$N+"echo."+$N+"$fileName %*"
+
+echo $batch > "$toolsPath\matrikon-explorer.bat"
