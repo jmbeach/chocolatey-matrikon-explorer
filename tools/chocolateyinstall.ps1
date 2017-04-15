43d1c0541d7fbf5f64bde1e6a7f5de904a4509e1
Jared Beach <Jared Beach>
2017-04-15 14:20:45 -0500

    removing alias code

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,11 +5,3 @@ $zipPath = "$toolsPath\$fileName"
 
 Get-ChocolateyUnzip -PackageName $packageName -FileFullPath $zipPath -Destination $toolsPath
 rm $zipPath -ea 0
-
-# create alias
-
-$N = "`r`n"
-
-$batch = "@echo off"+$N+"echo."+$N+"$fileName %*"
-
-echo $batch > "$toolsPath\matrikon-explorer.bat"
